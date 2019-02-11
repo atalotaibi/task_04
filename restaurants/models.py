5e0c7a5b8a6c4c76bfb2414922d5e1bda4031411
atalotaibi <alotaibiatallah@gmail.com>
2019-02-11 10:44:54 +0300

    Sultan & Atallah

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 100)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    def __str__(self):
+        return self.description
 # Create your models here.
